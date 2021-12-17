8bc3c203f0d3af9cdb471acfd8e5c5ee63450fed
Miguel Hernando <miguel.hernando@upm.es>
2021-12-17 14:25:59 +0100

    Update PRMdemos.py

--- PRMdemos.py
+++ PRMdemos.py
@@ -10,8 +10,14 @@ goal = (850,500)
 
 
 
+def reloadMap(map, init, goal):
+    map.loadMap(map, [init,goal])
+    map.draw()
+    map.draw_init_and_goal(init,goal)
+    pygame.display.update()
+    pass
 
-menu = ['''
+menu = '''
 PRM PLANNER DEMOS - Miguel Hernando
 Press any key to start:
 0 - Display Menu
@@ -36,38 +42,28 @@ if __name__ == '__main__':
         key = pygame_wait_for_key()
         if key != pygame.K_0: map.draw()
         if key == pygame.QUIT: break
-        if key == pygame.K_1: planner = rrt_simple
-        if key == pygame.K_2: planner = rrt_connect
-        if key == pygame.K_3: planner = rrt_star
-        if key == pygame.K_4: planner = rrt_dubbins
-        if key == pygame.K_5: Tree = TreeBase
-        if key == pygame.K_6: Tree = TreeDiscretized
+        if key == pygame.K_PLUS:
+            prm.populate(map,100)
+            prm.draw(map.canvas)
+            map.draw_init_and_goal(init,goal)
+
         if key == pygame.K_7: 
             map.createRandomMap(num_objects, [init,goal])
             map.draw()
             map.draw_init_and_goal(init,goal)
             pygame.display.update()
             continue
+        if key == pygame.K_7: 
+            reloadMap(map1, init, goal)
+            continue
         if key == pygame.K_8: 
-            map.loadMap(map1, [init,goal])
-            map.draw()
-            map.draw_init_and_goal(init,goal)
-            pygame.display.update()
+            reloadMap(map2, init, goal)
             continue
         if key == pygame.K_9: 
-            map.loadMap(map2, [init,goal])
-            map.draw()
-            map.draw_init_and_goal(init,goal)
-            pygame.display.update()
-            continue
-        if key == pygame.K_a: 
-            map.loadMap(map3, [init,goal])
-            map.draw()
-            map.draw_init_and_goal(init,goal)
-            pygame.display.update()
+            reloadMap(map3, init, goal)
             continue
         if key == pygame.K_0: 
             pygame_print_text(map.canvas, (100,80), menu, 20)
             continue
-        if not planner(map, init, goal): break
+        
     pygame.quit()

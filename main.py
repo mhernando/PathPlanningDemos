28ad6569c090468e72a9d1023901173abc2d605b
Miguel Hernando <miguel.hernando@upm.es>
2022-09-19 18:29:27 +0200

    gg
    gg

--- main.py
+++ main.py
@@ -1,27 +1,80 @@
 from RRTdemos import *
-if __name__ == '__main__':
+
+context = {
+    'event':False,
+    'key':None,
+    'planner': None,
+    'map': None
+    }
+
+def key_press(key):
+    c=key.char
+    print("Key pressed:", c)
+def sel():
+    global context
+    print(context['sel_planner'].get())
     
-    # Add a couple widgets. We're going to put pygame in `embed`.
-    root = tk.Tk()
-    embed = tk.Frame(root)
-    embed.pack()
+def init_gui_window():
+    global context
+    context['gui'] = root = tk.Tk()
+
     text = tk.Button(root, text='Blah.')
     text.pack()
-    root.update()
-
-
-    pygame.init()
-    #screen = pygame.display.set_mode((w,h))
     
+    context['sel_planner'] = var = tk.IntVar()
+    
+    
+    planners = [("Simple RRT", 101),
+                 ("RRT-Connect", 102),
+                 ("RRT-Star", 103),
+                 ("RRT (Dubbin's car)", 104)]
+
+
+    sel_frame = tk.Frame(root, borderwidth=1, relief=tk.RIDGE)
+    
+    for option, val in planners:
+        tk.Radiobutton(sel_frame, 
+                       text=option,
+                       padx = 20, 
+                       variable=var, 
+                       command=sel,
+                       value=val).pack(anchor=tk.W)
+    sel_frame.pack(padx=5, pady=5)
+    
+    context['sel_2'] = var2 = tk.IntVar()
+    sel_frame2 = tk.Frame(root,)
+    for option, val in planners:
+        tk.Radiobutton(sel_frame2, 
+                       text=option,
+                       padx = 20, 
+                       variable=var2, 
+                       command=sel,
+                       value=val).pack(anchor=tk.W)
+    sel_frame2.pack(padx=5, pady=5)
+    
+    root.bind('<KeyPress>',key_press)
+    root.update()
+    root.mainloop()
+def init_defaults():
+     global context
+     context['map'] = BaseMap(*map_size)
+     context['map'].loadMap(map1, [init,goal])
+     context['planner'] = rrt_simple
+
+
+if __name__ == '__main__':
+ 
+    pygame.init()
+    init_gui_window()
+    init_defaults()
    
-    map = BaseMap(*map_size) 
-    map.loadMap(map1, [init,goal])
-    planner = rrt_simple
+    map = context['map'] 
     map.draw()
     map.draw_init_and_goal(init,goal)
+    
     pygame_print_text(map.canvas, (100,80), menu, 20)
     pygame.display.update()
-
+    
     
     while(True):
         key = pygame_wait_for_key()
